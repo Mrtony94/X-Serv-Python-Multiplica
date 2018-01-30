ce21cfdc795a198e205b4340584ce342d2269b06
Antonio Andres Perez <aandres@gamma27.aulas.gsyc.urjc.es>
2018-01-30 10:47:21 +0100

    Tablas de multiplicar

---
+++ multiplica.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+for num1 in list(range(1,11)):
+    print("\nLa tabla de multiplicar del: ", num1)
+    for num2 in list(range(1,11)):
+        print(num1, '*', num2, "=", num1*num2)
